06955941ccf6415e12a58a30a63c376748dd7ea0
https://github.com/mikedent/mikedent.github.io.git <mike.dent@gmail.com>
2015-07-03 12:34:01 -0400

    Editing for needed modules

--- PSProfiles/Microsoft.PowerShell_profile.ps1
+++ PSProfiles/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,20 @@
 <#
 .SYNOPSIS
-    Microsoft.PowerShell_profile.ps1 - My PowerShell profile
+    Creates a customized version of powershell
 .DESCRIPTION
-    Microsoft.PowerShell_profile - Customizes the PowerShell console
+    Using multiple additional installed modules, creates a customized version of the powershell console.
+    The following additional modules are required:
+        vSphere PowerCLI - Tested with release 5.5+
+        PureStorage PowerShell Toolkit (https://github.com/barkz/PureStoragePowerShellToolkit)- Tested with release 2.8.0.430
+        NutanixCmdlets (Obtained from Nutanix Cluster or in Github repo) - Tested with release 1.1.2
+        Cisco UCS PowerTool (https://communities.cisco.com/docs/DOC-37154) - Tested with release 1.4.1
 .NOTES
     File Name   : Microsoft.PowerShell_profile.ps1
     Author      : Mike Dent
     Date        : 7/3/2015
 #>
 
-# Desired Module Loads below
-
+# Start of importing Modules
 # vSphere PowerCLI
 Import-Module -name VMware.VimAutomation.Cis.Core
 Import-Module -name VMware.VimAutomation.Core
@@ -18,14 +22,13 @@ Import-Module -name VMware.VimAutomation.HA
 Import-Module -name VMware.VimAutomation.SDK
 Import-Module -name VMware.VimAutomation.Storage
 Import-Module -name VMware.VimAutomation.Vds
-# PureStorage PowerShell 
+# PureStorage PowerShell
 Import-Module -name PureStoragePowerShell
-# NutanixCmdlets 
-#Import-Module "C:\Program Files (x86)\Nutanix Inc\NutanixCmdlets\Modules\Common\Common.dll"
-#Get-ChildItem -Path "C:\Program Files (x86)\Nutanix Inc\NutanixCmdlets\Modules" *.dll -recurse | ForEach-Object {Import-Module -Name $_.FullName -WarningAction silentlyContinue -Prefix "NTNX"}
-# Cisco UCS 
+# NutanixCmdlets
+Import-Module "C:\Program Files (x86)\Nutanix Inc\NutanixCmdlets\Modules\Common\Common.dll"
+Get-ChildItem -Path "C:\Program Files (x86)\Nutanix Inc\NutanixCmdlets\Modules" *.dll -recurse | ForEach-Object {Import-Module -Name $_.FullName -WarningAction silentlyContinue -Prefix "NTNX"}
+# Cisco UCS
 Import-Module -Name CiscoUCSPS
-
 # Finish Module Loads
 
 # Adding Custom Functions
@@ -44,9 +47,11 @@ function Reload-Profile {
             Write-Verbose "Running $_"
             . $_
         }
-    }    
+    }
 }
 
 function Edit-HostsFile {
     Start-Process -FilePath notepad -ArgumentList "$env:windir\system32\drivers\etc\hosts"
 }
+
+# End of custom functions
