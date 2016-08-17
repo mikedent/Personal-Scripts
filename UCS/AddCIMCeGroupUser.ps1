c36738969fca88fe30c7b4e5f4a6ee3cbd40c6f0
mikedent <mike.dent@gmail.com>
2016-08-16 22:44:00 -0400

    Adding CIMC scripts

---
+++ UCS/AddCIMCeGroupUser.ps1
@@ -0,0 +1,17 @@
+﻿#requires -Version 2.0 -Modules Cisco.IMC, Cisco.IMC, Cisco.IMC
+$adminUser = Read-Host -Prompt 'Enter an Admin CIMC User'
+$adminPassword = Read-Host -Prompt 'Enter password'| ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($adminUser, $adminPassword)
+$userAccount = 'egroup'
+$userPass = 'EG_Ad342'
+$IMC = @('10.94.0.11', '10.94.0.12', '10.94.0.13', '10.94.0.14', '10.94.0.15', '10.94.0.16')
+
+# Authenticate to IMC and set Time Settings
+foreach ($IMCHost in $IMC)
+{
+  Connect-Imc $IMCHost -Credential $cred
+  Set-ImcLocalUser -LocalUser 2 -AccountStatus active -Name $userAccount -Priv admin -Pwd $userPass -Force
+  Disconnect-Imc 
+}
+
+
--- UCS/SetCIMCNtpTimeZone.ps1
+++ UCS/SetCIMCNtpTimeZone.ps1
@@ -2,8 +2,8 @@
 $user = Read-Host -Prompt 'Enter an Admin CIMC User'
 $password = Read-Host -Prompt 'Enter password'| ConvertTo-SecureString -AsPlainText -Force
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($user, $password)
-$IMC = @('172.91.0.11', '172.91.0.12', '172.91.0.13', '172.91.0.14')
-$NTPServer = '10.255.255.249'
+$IMC = @('10.94.0.11', '10.94.0.12', '10.94.0.13', '10.94.0.14','10.94.0.15','10.94.0.16')
+$NTPServer = '10.255.255.225'
 
 # Authenticate to IMC and set Time Settings
 foreach ($IMCHost in $IMC)
