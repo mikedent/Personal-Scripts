fa92e5bff1a77a4844187bea9881f1eee9e856df
Mike Dent <mike.dent@gmail.com>
2019-07-08 13:38:36 -0500

    Updated IPMI IP Addresses

--- Lap-IPMI-Off.ps1
+++ Lap-IPMI-Off.ps1
@@ -2,9 +2,10 @@
 ipmitool -I lanplus -H 10.10.205.5 -U admin -P "admin" power soft
 
 # vSphere Nodes
-ipmitool -I lanplus -H 10.10.205.10 -U ADMIN -P "ADMIN" power  soft
+ipmitool -I lanplus -H 10.10.205.100 -U ADMIN -P "ADMIN" power  soft
+ipmitool -I lanplus -H 10.10.205.115 -U ADMIN -P "ADMIN" power  soft
 ipmitool -I lanplus -H 10.10.205.15 -U ADMIN -P "ADMIN" power  soft
-ipmitool -I lanplus -H 10.10.205.20 -U ADMIN -P "ADMIN" power  soft
+#ipmitool -I lanplus -H 10.10.205.20 -U ADMIN -P "ADMIN" power  soft
 ipmitool -I lanplus -H 10.10.205.25 -U ADMIN -P "ADMIN" power  soft
 ipmitool -I lanplus -H 10.10.205.30 -U ADMIN -P "ADMIN" power  soft
 
@@ -13,8 +14,3 @@ ipmitool -I lanplus -H 10.10.205.50 -U ADMIN -P "ADMIN" power soft
 ipmitool -I lanplus -H 10.10.205.55 -U ADMIN -P "ADMIN" power soft
 ipmitool -I lanplus -H 10.10.205.60 -U ADMIN -P "ADMIN" power soft
 
-# Rubrik Nodes
-ipmitool -I lanplus -H 10.10.205.70-U ADMIN -P "ADMIN" power soft
-ipmitool -I lanplus -H 10.10.205.71 -U ADMIN -P "ADMIN" power soft
-ipmitool -I lanplus -H 10.10.205.72 -U ADMIN -P "ADMIN" power soft
-ipmitool -I lanplus -H 10.10.205.73-U ADMIN -P "ADMIN" power soft
